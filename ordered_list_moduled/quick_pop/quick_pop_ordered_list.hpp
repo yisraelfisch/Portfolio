ad222bbdf5e708f381427cc82788a7699bc6ddb6
yisrael.fisch <yisraelsport@gmail.com>
2023-07-13 20:56:06 +0300

    thread safe quick pop ordered list

--- ordered_list_moduled/quick_pop/quick_pop_ordered_list.hpp
+++ ordered_list_moduled/quick_pop/quick_pop_ordered_list.hpp
@@ -2,24 +2,48 @@
  * Author: yisrael fisch            
  * File: quick_pop_ordered_list.hpp 
  * 
+ * Discription:
+ * this container is a sorted container
+ * the container can contain any type of data
+ * acording the ELEMENT you pass.
+ * the container will be sorted accorsing a_compare func 
+ * you will pass to the constructor.
+ * when you pop an element you will get the the top priority element
+ * in O(1)
  * ******************************/
 #pragma once
-#include <cstddef>
-#include <mutex>
-#include <condition_variable>
+#include <cstddef>//NULL
+#include <mutex>//mutex, unique_lock
+#include <condition_variable>// condition_variable 
 
 using namespace std;
 namespace myspace
 {
-
+// a templatic container that can hold any type of element
 template <typename ELEMENT>
 class Ordered_List
 {
 public:
+   /*   description: creates new doubly linked list 
+        params: typename ELEMENT, suitable compare func 
+        return value: ptr to this 
+        O(1)*/
     Ordered_List(bool (*compare_func)(const ELEMENT &lhs, const ELEMENT &rhs));
+    /*  description: release container resources
+        params: a suitable compare func 
+        O(n)*/
     ~Ordered_List();
+    /*  description: insert a new element to the contianer
+        params: new ELEMENT 
+        O(n) */
     void Push(const ELEMENT &element);
+    /*  description: pops the top priority ELEMENT
+        return value: the top priority ELEMENT
+        O(1) */
     ELEMENT Pop();
+    /*  description: check if the list is empty
+        return value: true if empty false if not
+        O(1) */
     bool IsEmpty();
 
 private:
@@ -30,7 +54,10 @@ private:
     bool (* m_compare_func)(const ELEMENT &lhs, const ELEMENT &rhs);
     mutex m_lock;
     condition_variable cond;
-};
+};//Ordered_List
+
+
+//------------------------------------Ordered_List func impl ------------------------
 
 template <typename ELEMENT>
 inline Ordered_List<ELEMENT>::Ordered_List(bool (*compare_func)(const ELEMENT &lhs, const ELEMENT &rhs)): m_compare_func(compare_func), m_head(NULL)
@@ -128,7 +155,7 @@ inline void Ordered_List<ELEMENT>::RemoveHead()
     m_head = m_head->next;
     delete temp;
 }
-//----------------------------------class node---------------------------------
+//----------------------------------class node & ctor impl---------------------------------
 template<typename ELEMENT>
 class Ordered_List<ELEMENT>::Node
 {
@@ -138,7 +165,7 @@ public:
     Node *next;
 
 
-};
+};//class node
 
 template<typename ELEMENT>
 inline Ordered_List<ELEMENT>::Node::Node(ELEMENT element):m_element(element), next(NULL)
